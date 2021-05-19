ebf13575f6db829eb7ffca39333a6d2fde19f54f
virarkh <rokhmahv@gmail.com>
2021-05-19 17:29:04 +0700

    Metode Cuci

--- application/models/metodecuci_model.php
+++ application/models/metodecuci_model.php
@@ -3,28 +3,28 @@
 class metodecuci_model extends CI_Model
 {
 
-	function indexMM()
+	function indexMM() // index metode
 	{
 		return $this->db->get('metode_mencuci')->result();
 	}
 
-	function addModelMM($data, $table)
+	function addModelMM($data, $table) // insert metode
 	{
 		$this->db->insert($table, $data);
 	}
 
-	function editModelMM($where, $table)
+	function editModelMM($where, $table) // get form edit 
 	{
 		return $this->db->get_where($table, $where);
 	}
 
-	function saveModelMM($where, $data, $table)
+	function saveModelMM($where, $data, $table) // proses edit
 	{
 		$this->db->where($where);
 		$this->db->update($table, $data);
 	}
 
-	function delModelMM($where, $table)
+	function delModelMM($where, $table) // delete metode
 	{
 		$this->db->where($where);
 		$this->db->delete($table);
